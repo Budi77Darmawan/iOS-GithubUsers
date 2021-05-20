aa0e2561487ae945115a67e904e2e8b7b4817d5e
Budi Darmawan <budi20.darmawan20@gmail.com>
2021-05-20 23:36:12 +0700

    add icon app, fixes layout profile and fixes layout cell table user

--- Github Users/Controller/ProfileViewController.swift
+++ Github Users/Controller/ProfileViewController.swift
@@ -11,7 +11,9 @@ class ProfileViewController: UIViewController {
 
     @IBOutlet weak var photoProfile: UIImageView!
     @IBOutlet weak var fullnameProfile: UILabel!
+    @IBOutlet weak var numberPhone: UILabel!
     @IBOutlet weak var cardStatus: UIView!
+    @IBOutlet weak var email: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,12 +24,9 @@ class ProfileViewController: UIViewController {
     }
     
     private func initCardStatus() {
-        cardStatus.backgroundColor = .white
         cardStatus.layer.shadowColor = UIColor.gray.cgColor
         cardStatus.layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
         cardStatus.layer.shadowRadius = 4.0
         cardStatus.layer.shadowOpacity = 0.4
-        cardStatus.layer.cornerRadius = 10.0
     }
-
 }
